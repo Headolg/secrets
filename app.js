22a17ec61820dd758fb7c1b674fe487ef69b3683
Headolg <fernando_pintoiu@yahoo.com>
2023-05-25 23:30:04 +0300

    update the call_back_url

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ passport.use(
   new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: process.env.CALL_BACK_URL,
   }, function (accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
